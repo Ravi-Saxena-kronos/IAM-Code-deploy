1ebcfdb69716ab72f38206386a4841faa99c7e0a
Ravi-Saxena-kronos <55644161+Ravi-Saxena-kronos@users.noreply.github.com>
2025-05-24 10:37:35 +0530

    Update BeforeInstall.sh

--- miscellaneous/Codedeploy/BeforeInstall.sh
+++ miscellaneous/Codedeploy/BeforeInstall.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-<<<<<<< HEAD
+export DEBIAN_FRONTEND=noninteractive
+echo '#!/bin/sh\nexit 101' > /usr/sbin/policy-rc.d
+chmod +x /usr/sbin/policy-rc.d
+apt-get update && apt-get install -y curl
+
+apt-get install -y rsync
 
 currentDir="$(dirname "$0")"
 COMPOSER_CMD=$(command -v composer)
-=======
-currentDir="$(dirname "$0")"
-COMPOSER_CMD=$(which composer)
->>>>>>> bcddde5 (1)
 CONF_DIR="$currentDir/conf"
 HTTPD_DIR="/etc/httpd"
 DEPLOYMENT_GROUP_NAME_ENV=$(echo "$DEPLOYMENT_GROUP_NAME" | cut -d- -f1)
 
-<<<<<<< HEAD
 # Detect package manager
 if command -v yum &> /dev/null; then
     PM="yum"
@@ -26,34 +26,47 @@ fi
 echo "Using package manager: $PM"
 
 #############################
-# Install Apache
+# Install PHP (7.3 or 8.3 depending on PM)
 #############################
-if [[ "$PM" == "yum" ]]; then
-    yum install -y httpd24 mod24_ssl
-elif [[ "$PM" == "apt-get" ]]; then
-    apt-get update
-    apt-get install -y apache2
-fi
 
-#############################
-# Install PHP 7.3
-#############################
 if [[ "$PM" == "yum" ]]; then
+    echo "Detected package manager: yum (likely Amazon Linux or RHEL/CentOS)"
     yum remove -y php73 || true
-    yum install -y php73-fpm php73-mbstring php73-gd php73-cli php73-mcrypt php73-opcache php73-pdo php73-mysqlnd php73-zip
+    yum install -y \
+        php73-fpm \
+        php73-mbstring \
+        php73-gd \
+        php73-cli \
+        php73-mcrypt \
+        php73-opcache \
+        php73-pdo \
+        php73-mysqlnd \
+        php73-zip
+
 elif [[ "$PM" == "apt-get" ]]; then
-    add-apt-repository ppa:ondrej/php -y
+    echo "Detected package manager: apt-get (likely Ubuntu/Debian)"
+    apt-get update
+    apt-get install -y software-properties-common lsb-release ca-certificates apt-transport-https
+
+    # Add PHP 8.3 PPA only if not already added
+    if ! grep -q "^deb .*.ondrej.*php" /etc/apt/sources.list /etc/apt/sources.list.d/* 2>/dev/null; then
+        add-apt-repository ppa:ondrej/php -y
+    fi
+
     apt-get update
     apt-get install -y \
-  php8.4 \
-  php8.4-fpm \
-  php8.4-mbstring \
-  php8.4-gd \
-  php8.4-cli \
-  php8.4-opcache \
-  php8.4-pdo \
-  php8.4-mysql \
-  php8.4-zip
+        php8.3 \
+        php8.3-fpm \
+        php8.3-mbstring \
+        php8.3-gd \
+        php8.3-cli \
+        php8.3-opcache \
+        php8.3-pdo \
+        php8.3-mysql \
+        php8.3-zip
+else
+    echo "Unsupported package manager: $PM"
+    exit 1
 fi
 
 #############################
@@ -66,67 +79,25 @@ $PM install -y git
 #############################
 if [[ -z "$COMPOSER_CMD" ]]; then
     curl -sS https://getcomposer.org/installer | php
-=======
-#############################
-#Install Apache(httpd24)
-#############################
-yum install -y mod24_ssl
-
-#############################
-#Install php (7.3)
-#############################
-yum remove -y php73
-yum install -y php73-fpm php73-mbstring php73-gd php73-cli php73-mcrypt php73-opcache php73-pdo php73-mysqlnd php73-zip
-yum install -y git --nogpgcheck
-
-#############################
-#Install composer
-#############################
-if [[ "" == "$COMPOSER_CMD" ]]
-then
-    curl -sS https://getcomposer.org/installer | sudo php
->>>>>>> bcddde5 (1)
     mv composer.phar /usr/local/bin/composer
     ln -s /usr/local/bin/composer /usr/bin/composer
 fi
 
-<<<<<<< HEAD
+apt-get install -y apache2
+
 #############################
 # Apache Config Files
 #############################
-rsync -azq "$CONF_DIR/apache/conf.d/" "$HTTPD_DIR/conf.d/"
-rsync -azq "$CONF_DIR/apache/conf.modules.d/" "$HTTPD_DIR/conf.modules.d/"
+rsync -azq "miscellaneous/Codedeploy/conf/apache/conf.d/" "/etc/apache2/conf-available/"
+rsync -azq "miscellaneous/Codedeploy/conf/apache/conf.modules.d/" "/etc/apache2/mods-available/"
 
 #############################
 # PHP Config
 #############################
-cp -f "$CONF_DIR/php/php-7.0.ini" /etc/php.ini
+cp -f "miscellaneous/Codedeploy/conf/php/php-7.0.ini" /etc/php.ini
 
 #############################
 # PHP-FPM Config
 #############################
-cp -f "$CONF_DIR/php-fpm/$DEPLOYMENT_GROUP_NAME_ENV/www.conf" /etc/php-fpm.d/www.conf
-=======
-##################################################################
-
-
-#Copy conf.d & conf.modules.d files
-rsync -azq $CONF_DIR/apache/conf.d/ /etc/httpd/conf.d/
-rsync -azq $CONF_DIR/apache/conf.modules.d/ /etc/httpd/conf.modules.d/
-
-##################################################################
-
-###########
-#PHP
-###########
-cp -f $CONF_DIR/php/php-7.0.ini /etc/php-7.0.ini
-
-##################################################################
-
-###########
-#PHP-FPM
-###########
-cp -f $CONF_DIR/php-fpm/$DEPLOYMENT_GROUP_NAME_ENV/www.conf /etc/php-fpm.d/www.conf
-
-##################################################################
->>>>>>> bcddde5 (1)
+cp -f "miscellaneous/Codedeploy/php-fpm/$DEPLOYMENT_GROUP_NAME_ENV/www.conf" /etc/php-fpm.d/www.conf
+echo "This is a message"
